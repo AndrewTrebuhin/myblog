791b8c519bbaccf665f1edb870f070ce73a236c5
Andrew Trebuhin <andrewtrebuhin@gmail.com>
2014-11-06 16:33:37 +0200

    Fix commit

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    login "Michael Hartl"
-    email "michael@example.com"
+    sequence(:login) { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com" }
     password 'password'
     password_confirmation 'password'
+
+    factory :admin do
+      admin true
+    end
   end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -33,6 +33,7 @@ RSpec.describe "AuthenticationPages", :type => :request do
       before { sign_in user }
 
       it { should have_title(user.login) }
+      it { should have_link('Users',       href: users_path) }
       it { should have_link('Profile',     href: user_path(user)) }
       it { should have_link('Settings',    href: edit_user_path(user)) }
       it { should have_link('Sign out',    href: signout_path) }
@@ -66,6 +67,18 @@ RSpec.describe "AuthenticationPages", :type => :request do
         end
       end
 
+      describe "as non-admin user" do
+        let(:user) { FactoryGirl.create(:user) }
+        let(:non_admin) { FactoryGirl.create(:user) }
+
+        before { sign_in non_admin, no_capybara: true }
+
+        describe "submitting a DELETE request to the User#destroy" do
+          before { delete user_path(user) }
+          specify { expect(response).to redirect_to(root_url) }
+        end
+      end
+
       describe "in the Users controller" do
 
         describe "visiting the edit page" do
@@ -76,6 +89,11 @@ RSpec.describe "AuthenticationPages", :type => :request do
         describe "submitting to the update action" do
           before { patch user_path(user) }
           specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_title('Sign in') }
         end
       end
     end
